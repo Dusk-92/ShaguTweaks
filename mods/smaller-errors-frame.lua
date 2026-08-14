2917951b7cd9ae102e1136924508a21c83b0ac39
Dusk-92 <romain.thomas92@proton.me>
2026-08-14 14:58:33 +0200

    Add files via upload

---
+++ mods/smaller-errors-frame.lua
@@ -0,0 +1,13 @@
+local T = ShaguTweaks.T
+
+local module = ShaguTweaks:register({
+  title = T["Smaller Error Frame"],
+  description = T["Resizes the error frame to 1 line instead of 3."],
+    expansions = { ["vanilla"] = true },
+    category = T["General"],
+    enabled = nil,
+})
+
+module.enable = function(self)
+    UIErrorsFrame:SetHeight(UIErrorsFrame:GetHeight() / 3)
+end
